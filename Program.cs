97ae238f86b94296eda8198ec0ed26e10f1df318
Tanvi Teli <uittt@leeds.ac.uk>
2024-12-16 09:45:55 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RockPaperScissor
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
